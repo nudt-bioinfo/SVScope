18c8e3bf9053d317d0443b5bb8300f2e4e564d33
liyilin <166374177+liyilingithub@users.noreply.github.com>
2025-07-21 10:42:33 +0800

    Update utilities.py

--- SVScope_filter_src/utilities.py
+++ SVScope_filter_src/utilities.py
@@ -15,7 +15,7 @@ resize = torchvision.transforms.Resize([hight, hight])
 
 
 def mymkdir(mydir):
-    if not os.path.exists(mydir):  # 判断是否存在文件夹如果不存在则创建为文件夹
+    if not os.path.exists(mydir):  
         os.makedirs(mydir)
 
 class IdentifyDataset(torch.utils.data.Dataset):
@@ -44,7 +44,6 @@ class IdentifyDataset(torch.utils.data.Dataset):
             return torch.load(self.path + "dataset/n/" + self.nfile_list[index])
 
 def kernel_cigar(read, ref_min, ref_max, ciagr_resize, zoom):
-    # Cue代码融合如下：（新增reads配对特征，新增图像通道数：RRLL/RL/LR三个通道）
     # Read-pair types
     SV_SIGNAL_RP_TYPE = Enum("SV_SIGNAL_RP_TYPE", 'LR LLRR RL')
 
@@ -61,30 +60,27 @@ def kernel_cigar(read, ref_min, ref_max, ciagr_resize, zoom):
 
     # Provide a default value for read_pair_type
     read_pair_type = None
-    # 新增代码到此为止
 
-    # todo: 修改了源码 将4通道变成7通道
     cigars_img = torch.zeros([7, int((ref_max - ref_min) / zoom)])
 
     max_terminal = read.reference_start - ref_min
 
     for operation, length in read.cigar:  # (operation{10 class}, length)
-        # 新增代码如下：
+：
         read_pair_type = get_read_pair_type(read)
-        # 代码结束
-        if operation == 0: #代表匹配或不匹配
+        if operation == 0: 
             cigars_img[0, int(max_terminal / zoom):int((max_terminal + length) / zoom)] = 255
             max_terminal += length
-        elif operation == 2: #代表删除
+        elif operation == 2: 
             cigars_img[1, int(max_terminal / zoom):int((max_terminal + length) / zoom)] = 255
             max_terminal += length
-        elif operation == 1: #代表插入
+        elif operation == 1: 
             cigars_img[2, int((max_terminal - length / 2) / zoom):int((max_terminal + length / 2) / zoom)] = 255
-        elif operation == 4: #代表软剪切
+        elif operation == 4: 
             cigars_img[3, int((max_terminal - length / 2) / zoom):int((max_terminal + length / 2) / zoom)] = 255
         elif operation == 3 or operation == 7 or operation == 8:
             max_terminal += length
-    # 新增补充代码如下：
+
     if read_pair_type is not None:
 
         max_terminal = read.reference_start - ref_min
@@ -102,12 +98,12 @@ def kernel_cigar(read, ref_min, ref_max, ciagr_resize, zoom):
     else:
         pass
         # print("Warning: read_pair_type was not determined for this read.")
-    # 到此为止
+
 
     return ciagr_resize(cigars_img.unsqueeze(1))
 
 
-# todo 去除I的影响以对齐ref alignment # todo
+
 def cigar_new_img_single_optimal(bam_path, chromosome, begin, end, zoom):
     # print("======= cigar_img_single begin =========")
     r_start = []
@@ -115,15 +111,15 @@ def cigar_new_img_single_optimal(bam_path, chromosome, begin, end, zoom):
     sam_file = pysam.AlignmentFile(bam_path, "rb")
 
     for read in sam_file.fetch(chromosome, begin, end):
-        # 过滤掉 reference_start 和 reference_end 为 None 的 reads
+
         if read.reference_start is not None and read.reference_end is not None:
             r_start.append(read.reference_start)
             r_end.append(read.reference_end)
-        # 新增代码结束
+
     if r_start :
         ref_min = np.min(r_start)
         ref_max = np.max(r_end)
-        cigars_img = torch.empty([7, len(r_start), hight]) # todo 同样修改了代码维度4
+        cigars_img = torch.empty([7, len(r_start), hight])
         ciagr_resize = torchvision.transforms.Resize([1, hight])
 
         for i, read in enumerate(sam_file.fetch(chromosome, begin, end)):
@@ -132,7 +128,7 @@ def cigar_new_img_single_optimal(bam_path, chromosome, begin, end, zoom):
 
         cigars_img = resize(cigars_img)
     else:
-        cigars_img = torch.zeros([7, hight, hight]) #  todo 同样修改了代码4
+        cigars_img = torch.zeros([7, hight, hight]) 
 
     sam_file.close()
     # print("======= to input image end =========")
